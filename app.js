60fa71ce1b53c1cc7b70bc00c11c90d813f57f74
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-21 18:48:41 +0530

    code push

--- app.js
+++ app.js
@@ -5,15 +5,22 @@
 
 // prinatNmae();
 
-function rollDice() {
-    let rand = Math.floor( Math.random() * 6 ) + 1;
-    console.log(rand);
+// function rollDice() {
+//     let rand = Math.floor( Math.random() * 6 ) + 1;
+//     console.log(rand);
+// }
+
+// rollDice();
+
+// function printGame(game) {
+//     console.log(game);
+// }
+
+// printGame("cricket");
+
+function calAvg(a, b, c) {
+    let avg = (a+b+c)/3;
+    console.log(avg);
 }
 
-rollDice();
-
-function printGame(game) {
-    console.log(game);
-}
-
-printGame("cricket");
+calAvg(3, 5, 8);
